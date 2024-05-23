e893c18d9f19d77a22791bf2fb873b077b010894
Sebastian Vinay Muñoz Diaz <sebastian.munoz@neoris.com>
2024-05-23 06:28:16 -0500

    updating all repository, adding Test Parallel, adding cucumber summary

--- src/test/java/backend/testParallel.java
+++ src/test/java/backend/testParallel.java
@@ -1,39 +1,34 @@
 package backend;
 
-import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
-
+import org.junit.jupiter.api.Test;
+import com.intuit.karate.Results;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 
+
 public class testParallel {
 
     @Test
+
     public void testParallel() {
-        Results results = Runner.path("classpath:backend/user").parallel(5);
-        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        Results results = Runner.path("classpath:backend/user").outputCucumberJson(true).parallel(1);
         generateReport(results.getReportDir());
     }
 
     public static void generateReport(String karateOutputPath) {
-        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
         List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "demo");
+        Configuration config = new Configuration(new File("target"), "reqResApiAutomation");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
-
 }
